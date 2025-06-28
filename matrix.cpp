a0916df7227d9081114059fcae38b63ce538fa68
Akshita <akshita10656jain@gmail.com>
2025-06-28 23:45:03 +0530

    add matrix code

---
+++ matrix.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Matrix {
+int A[10];
+Int n;
+};
+void set( struct Matrix m, int i,int j,int x){
+if(i==j){
+m->[i-1]=x;
+}
+}
+int get(struct Matrix m, int i,int j){
+if(i==j) 
+return m.A[i-1];
+else
+return 0;}
+
+void Display(struct Matrix m){
+for (int i=0;i<m.n;i++){
+for(int j=0;j<m.n;j++){
+if(i==j)
+printf (“%d”,m.A[i-1]);
+else
+printf(“0 ”);
+}
+printf(“\n”);
+}
+}
+int main(){
+struct matrix m;
+m.n=4;
+set(&m,1,1,5);set(&m,2,2,9);set(&m,3,3,8);set(&m,4,4,12);
+Display(m);
+printf(“%d \n”, Get(m,2,2));
+return 0;
+}
