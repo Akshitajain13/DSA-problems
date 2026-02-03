2a742f8542e9b19967d8b1c247c3869b82698ea9
Akshita <akshita10656jain@gmail.com>
2026-02-03 23:58:38 +0530

    equal Sum Partition using tabulation

---
+++ equalSumPartition.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:  int t[204][10006];
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n=arr.size();
+        for (int i=0;i<n+1;i++){
+            for (int j=0;j<sum+1;j++){
+                if(i==0){
+                    t[i][j]=false;
+                }
+                if(j==0){
+                    t[i][j]=true;
+                }                
+            }
+        }
+        for (int i=1;i<n+1;i++){
+            for (int j=1;j<sum+1;j++){
+                if(arr[i-1]<=j){
+                    t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+         return t[n][sum];
+    }
+    bool equalPartition(vector<int>& arr) {
+        // code here
+        int sum=0;
+        int n=arr.size();
+        for (int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        return isSubsetSum(arr, sum / 2);
+        
+    }
+};
